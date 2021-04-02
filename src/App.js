6954f59ec800b71bcde2681b7345b80c334bbe06
shellyyg <chih-ching.yang@whu.edu>
2021-04-02 16:12:11 +0800

    change aws access permission level

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          "changes from feature branch!"
+          "change access key permission level!"
         </p>
         <a
           className="App-link"
